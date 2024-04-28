7b8dd507efda53084a249e69e2d214e21e347e23
nhqb1010 <quocbao102000@gmail.com>
2024-04-28 16:19:23 +0700

    🦁 setup: clone vue template for client

---
+++ client/src/components/Counter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useCounterStore } from "../stores/counter";
+import { storeToRefs } from "pinia";
+
+const counterStore = useCounterStore();
+const { count, doubleCount } = storeToRefs(counterStore);
+</script>
+
+<template>
+    <div class="counter__container">
+        <h2>Counter</h2>
+        <p>Count: {{ count }}</p>
+        <p>Double Count: {{ doubleCount }}</p>
+        <button @click="counterStore.increment()">Increment</button>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Counter from "../components/Counter.vue";
+</script>
+
+<template>
+    <main>
+        <h1>This is from the home page</h1>
+
+        <!-- Counter Section -->
+        <Counter />
+    </main>
+</template>
+
+<style scoped>
+main {
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
