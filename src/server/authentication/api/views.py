d84f138de1b252340ea026b037881eeb5f46a381
nhqb1010 <quocbao102000@gmail.com>
2024-05-02 14:57:58 +0700

    🐺 feat: login/verify token

--- src/server/authentication/api/views.py
+++ src/server/authentication/api/views.py
@@ -1,11 +1,36 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenVerifySerializer
+from rest_framework_simplejwt.exceptions import TokenError
 
-from ..serializers import LoginSerializer
+from app.errors.error_codes import AuthErrorCodes
+from app.errors.exceptions import AppAuthException
+from app.errors.utils import format_error_response
 
 
 @api_view(['POST'])
 def login_view(request):
-    serializer = LoginSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    return Response(serializer.validated_data)
+    serializer = TokenObtainPairSerializer(data=request.data)
+
+    try: 
+        serializer.is_valid()
+    except AuthenticationFailed:
+        error = AppAuthException(AuthErrorCodes.INVALID_LOGIN_CREDENTIALS)
+        return Response(format_error_response(error), status=error.status_code)
+    
+    return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def verify_token_view(request):
+    serializer = TokenVerifySerializer(data=request.data)
+
+    try:
+        serializer.is_valid()
+    except TokenError as _:
+        error = AppAuthException(AuthErrorCodes.INVALID_TOKEN)
+        return Response(format_error_response(error), status=error.status_code)
+
+    return Response(serializer.validated_data, status=status.HTTP_200_OK)
--- src/server/authentication/urls.py
+++ src/server/authentication/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from .api.views import login_view
+from .api.views import login_view, verify_token_view
 
 
 urlpatterns = [
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("api/auth/token/", login_view, name="token_obtain_pair"),
     path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/auth/token/verify/", verify_token_view, name="token_verify"),
 ]
